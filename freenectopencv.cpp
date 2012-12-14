9581e8f134c4347af630a908e981a5b00f683487
Mukul <mukul@mukul-laptop.(none)>
2012-12-14 00:23:59 -0800

    cleaned up the code for submission

--- freenectopencv.cpp
+++ freenectopencv.cpp
@@ -1,6 +1,18 @@
+/************************************************************
+ * Mukul Murthy, Jordan Smith, Tim Brown, and Kunal Agarwal *
+ * UC Berkeley EECS 149 - Embedded Systems                  *
+ * Fall 2012                                                *
+ * Instructors: Edward Lee, Sanjit Seshia                   *
+ * Mentor: Zach Wasson                                      *
+ *							    *
+ * BROBOT VISION SYSTEM					    * 
+ * **********************************************************/
+
+// Sources we borrowed code from:
 // Skeleton: http://fahmifahim.com/2011/05/16/kinect-and-opencv/
 // Tracking colored objects: http://www.aishack.in/2010/07/tracking-colored-objects-in-opencv/
 // Tracking ball: http://projectproto.blogspot.com/2012/04/android-opencv-object-tracking.html
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -13,6 +25,7 @@
 #include <cv.h>
 #include <highgui.h>
 
+// Constants for image size
 #define FREENECTOPENCV_WINDOW_D "Depthimage"
 #define FREENECTOPENCV_WINDOW_N "Normalimage"
 #define FREENECTOPENCV_RGB_DEPTH 3
@@ -22,6 +35,7 @@
 #define FREENECTOPENCV_DEPTH_WIDTH 640
 #define FREENECTOPENCV_DEPTH_HEIGHT 480
 
+// Constants we use in our algorithms and models
 #define PI 3.14159265
 #define ALPHA .7
 #define DEPTH_MIN_DIST 23.0
@@ -35,23 +49,35 @@
 #define X1_COEFF 4.844920
 #define X0_COEFF 240.279
 
+// Image arrays
 IplImage* depthimg = 0;
 IplImage* rgbimg = 0;
 IplImage* tempimg = 0;
 IplImage* red_img = 0;
 IplImage* orange_img = 0;
 IplImage* canny_temp = 0;
+
+// Raw distance to a cup, in 8-bit depth units
 uint8_t cupdist = 0;
+// Center of cup
 uint16_t centX = 0;
 uint16_t centY = 0;
+// Distance to cup, in inches
 double inches = 0.0;
+// Distance to cup in the previous frame. Used for filtering.
 double prevInches = 0.0;
+// Displacement from the center line, in inches
 double offsetInches = 0.0;
+// Displacement from center line in previous frame. For filtering.
 double prevOffsetInches = 0.0;
+// The last distance and displacement before a drastic change.
 double stableInches = 0.0;
 double stableOffsetInches = 0.0;
+// Number of frames since a cup was removed.
 int keyFrameCount = 0;
+// Number of consecutive frames we think we've seen a hit. 
 unsigned short hitCount = 0;
+// Mutexes for depth and RGB images, to make sure each arry is one frame.
 pthread_mutex_t mutex_depth = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_rgb = PTHREAD_MUTEX_INITIALIZER;
 pthread_t cv_thread;
@@ -61,11 +87,9 @@ void depth_cb(freenect_device *dev, void *depth, uint32_t timestamp)
 {
         cv::Mat depth8;
         cv::Mat mydepth = cv::Mat( FREENECTOPENCV_DEPTH_WIDTH,FREENECTOPENCV_DEPTH_HEIGHT, CV_16UC1, depth);
-
         mydepth.convertTo(depth8, CV_8UC1, 1.0/4.0);
         pthread_mutex_lock( &mutex_depth );
         memcpy(depthimg->imageData, depth8.data, 640*480);
-        // unlock mutex
         pthread_mutex_unlock( &mutex_depth );
 
 }
@@ -73,24 +97,21 @@ void depth_cb(freenect_device *dev, void *depth, uint32_t timestamp)
 // callback for rgbimage, called by libfreenect
 void rgb_cb(freenect_device *dev, void *rgb, uint32_t timestamp)
 {
-        // lock mutex for opencv rgb image
         pthread_mutex_lock( &mutex_rgb );
         memcpy(rgbimg->imageData, rgb, FREENECT_VIDEO_RGB_SIZE);
-        // unlock mutex
         pthread_mutex_unlock( &mutex_rgb );
 }
 
-// Looks for red cups
+// Takes in an BGR image and returns a smoothed binary image of red objects.
 IplImage* getRedImage(IplImage* img)
 {
-	// Convert the image into an HSV image
 	IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
 	cvCvtColor(img, imgHSV, CV_BGR2HSV);
 	IplImage* imgThreshed = cvCreateImage(cvGetSize(img), 8, 1);
 	IplImage* redHigh = cvCreateImage(cvGetSize(img), 8, 1);
 	IplImage* redLow = cvCreateImage(cvGetSize(img), 8, 1);
-	cvInRangeS(imgHSV, cvScalar(165, 170, 45), cvScalar(255, 255, 255), redHigh);
-	cvInRangeS(imgHSV, cvScalar(0, 170, 45), cvScalar(6, 255, 255), redLow);
+	cvInRangeS(imgHSV, cvScalar(165, 130, 45), cvScalar(255, 255, 255), redHigh);
+	cvInRangeS(imgHSV, cvScalar(0, 120, 45), cvScalar(6, 255, 255), redLow);
 	cvOr(redHigh, redLow, imgThreshed);
 	cvReleaseImage(&imgHSV);
 	cvReleaseImage(&redHigh);
@@ -99,17 +120,15 @@ IplImage* getRedImage(IplImage* img)
 	return imgThreshed;
 }
 
-// Looks for orange ball
+// Takes in an BGR image and returns a smoothed binary image of red objects.
 IplImage* getOrangeImage(IplImage* img)
 {
-	// Convert the image into an HSV image
 	IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
 	cvCvtColor(img, imgHSV, CV_BGR2HSV);
 	IplImage* imgThreshed = cvCreateImage(cvGetSize(img), 8, 1);
 	cvInRangeS(imgHSV, cvScalar(10, 170, 50), cvScalar(17, 255, 255), imgThreshed);
 	cvReleaseImage(&imgHSV);
 	cvSmooth(imgThreshed, imgThreshed, CV_MEDIAN, 7);
-	//cvSmooth(imgThreshed, imgThreshed, CV_GAUSSIAN, 9, 9);
 	return imgThreshed;
 }
 
@@ -135,22 +154,19 @@ void *cv_threadfunc (void *ptr) {
         // use image polling
         while (1) {
 		keyFrameCount++;
-                //lock mutex for depth image
+
+		// Display the depth and color images
                 pthread_mutex_lock( &mutex_depth );
-                // show image to window
                 cvCanny(depthimg, canny_temp, 50.0, 200.0, 3);
 		cvCvtColor(depthimg,tempimg,CV_GRAY2BGR);
-                //cvCvtColor(tempimg,tempimg,CV_HSV2BGR);
-
                 cvShowImage(FREENECTOPENCV_WINDOW_D,depthimg);
-                //unlock mutex for depth image
                 pthread_mutex_unlock( &mutex_depth );
-
-                //lock mutex for rgb image
                 pthread_mutex_lock( &mutex_rgb );
-                // show image to window
                 cvCvtColor(rgbimg,tempimg,CV_BGR2RGB);
                 cvShowImage(FREENECTOPENCV_WINDOW_N, tempimg);
+                pthread_mutex_unlock( &mutex_rgb );
+
+		//Threshold the red and orange images
 		red_img = getRedImage(tempimg);
 		orange_img = getOrangeImage(tempimg);
 		cvShowImage("CUP CAM", red_img);
@@ -159,16 +175,15 @@ void *cv_threadfunc (void *ptr) {
 
 		// Canny filter
 		cvCanny(red_img, red_img, 50.0, 200.0, 3);
-		//cvShowImage("Canny Image", red_img);
 		cv::Mat cups(red_img);
-		//cv::smooth(cups, cups);
+		// This will hold the array of cup contours.
 		std::vector<std::vector <cv::Point> > cupContours;
 		std::vector<cv::Vec4i> cupHierarchy;
 		cv::findContours(cups, cupContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point());
-		//cv::boxFilter(cups, cups, -1, cv::Size(3, 3));
-		//cv::imshow("Canny Image", cups);
 		cv::Scalar color = cv::Scalar(100,100,100);
 		cv::Mat drawing = cv::Mat::zeros(cups.size(), CV_8UC3 );
+
+		// Calculate the biggest red object
 	        double biggestSize = -1;
 		int biggestContour = -1;	
 		for(unsigned int i = 0; i < cupContours.size(); i++ )
@@ -179,9 +194,10 @@ void *cv_threadfunc (void *ptr) {
 				biggestSize = s;
 				biggestContour = i;
 			}
-			//drawContours(drawing, cupContours, i, color, 2, 8, cupHierarchy, 0, cv::Point());
 		}
+
 		cv::Rect cupRect;
+		// If one contour was found, handle cup distance
 		if (cupContours.size() > 0)
 		{
 			cupRect = cv::boundingRect(cv::Mat(cupContours[biggestContour]));
@@ -193,6 +209,7 @@ void *cv_threadfunc (void *ptr) {
 			if ((inches > DEPTH_MIN_DIST) && (inches < DEPTH_MAX_DIST)) {
 				inches = ALPHA * prevInches + (1-ALPHA) * inches;
 				offsetInches =  ALPHA * prevOffsetInches + (1-ALPHA) * offsetInches;
+				// If distance or offset is different, cup might have been removed.
 				if ((keyFrameCount > FRAMES_TO_CONVERGENCE) 
 				    && ((abs(inches - stableInches) > DISTANCE_THRESHOLD)
 					|| (abs(offsetInches - stableOffsetInches > OFFSET_THRESHOLD)))) {
@@ -207,8 +224,9 @@ void *cv_threadfunc (void *ptr) {
 					keyFrameCount = 0;
 					hitCount = 0;
 				}
-				else
+				else {
 					printf("Cup dist %f offset %f\n", inches, offsetInches);
+				}
 				prevInches = inches;
 				prevOffsetInches = offsetInches;
 			}
@@ -222,6 +240,7 @@ void *cv_threadfunc (void *ptr) {
 		std::vector<cv::Vec4i> ballHierarchy;
 		cv::findContours(balls, ballContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point());
 		drawing = cv::Mat::zeros(cups.size(), CV_8UC3 );
+		// Find the biggest orange object
 	        biggestSize = -1;
 		biggestContour = -1;	
 		for(unsigned int i = 0; i < ballContours.size(); i++ )
@@ -241,8 +260,6 @@ void *cv_threadfunc (void *ptr) {
 		drawContours( drawing, ballContours, biggestContour, color, 2, 8, ballHierarchy, 0, cv::Point());
 		cv::imshow("Ball Contours", drawing);
 
-                //unlock mutex
-                pthread_mutex_unlock( &mutex_rgb );
 
                 // wait for quit key
                 if( cvWaitKey( 15 )==27 )
@@ -276,7 +293,9 @@ int main(int argc, char **argv)
         freenect_set_depth_callback(f_dev, depth_cb);
         freenect_set_video_callback(f_dev, rgb_cb);
         freenect_set_video_format(f_dev, FREENECT_VIDEO_RGB);
+	// Tilt the physical Kinect device to 0 degrees. 
 	freenect_set_tilt_degs(f_dev, 0);
+
         // create opencv display thread
         res = pthread_create(&cv_thread, NULL, cv_threadfunc, (void*) depthimg);
         if (res) {
